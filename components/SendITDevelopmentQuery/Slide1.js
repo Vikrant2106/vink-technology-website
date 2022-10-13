1173f0c11d97ce8226a01fbc5381e278f44cbf3a
vikant2106 <vikratitpro@gmail.com>
2022-10-13 11:06:12 +0530

    new push done

--- components/SendITDevelopmentQuery/Slide1.js
+++ components/SendITDevelopmentQuery/Slide1.js
@@ -10,28 +10,29 @@ export default function Slide1({ slug }) {
               <img src="/img/contact-img.png" alt="Images" />
             </div>
           </div>
-          <div
-            className="col-md-6"
-            style={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <div class="row">
-              <div class="col-lg-12 col-sm-12">slpe</div>
-            </div>
-            <div class="col-lg-12 col-sm-12">
-              <div class="form-group">
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  class="form-control"
-                  required=""
-                  data-error="Please enter your name"
-                  placeholder="Your Name*"
-                />
-                <div class="help-block with-errors"></div>
+
+          <div className="col-md-6">
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                height: "100%",
+              }}
+            >
+              <div class="col-lg-12 col-sm-12">
+                <div class="form-group">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    class="form-control"
+                    required=""
+                    data-error="Please enter your name"
+                    placeholder="Your Name*"
+                  />
+                  <div class="help-block with-errors"></div>
+                </div>
+                <div />
               </div>
             </div>
           </div>
